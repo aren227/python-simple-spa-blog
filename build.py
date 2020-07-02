9e28a8b67ad08d0f5f357a0e5080199cf07338b4
aren227 <mathdong01@gmail.com>
2020-07-02 16:09:44 +0900

    Require posts and template folder path

--- build.py
+++ build.py
@@ -2,32 +2,45 @@ import os
 from pathlib import Path
 import markdown2
 import json
+import sys
+import shutil
 
 from post import Post
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Please provide post and template folder path.")
+        print("build.py <POST_FOLDER_PATH> <TEMPLATE_FOLDER_PATH>")
+        exit()
 
-    if not os.path.isdir("posts"):
-        print("Folder 'posts' not found.")
+    POSTS_FOLDER = sys.argv[1]
+    TEMPLATE_FOLDER = sys.argv[2]
+
+    if not os.path.isdir(POSTS_FOLDER):
+        print("Posts folder not found: " + POSTS_FOLDER)
+        exit()
+
+    if not os.path.isdir(TEMPLATE_FOLDER):
+        print("Template folder not found: " + TEMPLATE_FOLDER)
         exit()
 
     posts = []
     meta_files = []
 
-    for file in os.listdir("posts"):
+    for file in os.listdir(POSTS_FOLDER):
         if file.endswith(".md") and file != "README.md":
-            posts.append(Post(os.path.join("posts", file)))
+            posts.append(Post(os.path.join(POSTS_FOLDER, file)))
             meta_files.append(file + ".meta")
 
-    for file in os.listdir("posts"):
+    for file in os.listdir(POSTS_FOLDER):
         if file.endswith(".meta") and file not in meta_files:
-            os.remove(os.path.join("posts", file))
+            os.remove(os.path.join(POSTS_FOLDER, file))
 
     posts = sorted(posts, key=lambda x: x.creation_time, reverse=True)
 
     print(len(posts), "posts were found")
 
-    Path("result/posts").mkdir(parents=True, exist_ok=True)
+    Path("__posts").mkdir(exist_ok=True)
 
     tags = set()
     for post in posts:
@@ -66,7 +79,7 @@ if __name__ == '__main__':
 
         jsonObj["posts"].append(postObj)
 
-    json_file = open("result/posts.json", mode='w', encoding='utf-8')
+    json_file = open("posts.json", mode='w', encoding='utf-8')
     json_file.write(json.dumps(jsonObj))
     json_file.close()
     print("Done!")
@@ -76,8 +89,16 @@ if __name__ == '__main__':
 
         html = markdown2.markdown(post.content)
 
-        html_file = open(os.path.join("result/posts", post.id + ".html"), mode='w', encoding='utf-8')
+        html_file = open(os.path.join("__posts", post.id + ".html"), mode='w', encoding='utf-8')
         html_file.write(html)
         html_file.close()
 
         print("Done!")
+
+    print("Placing template files...", end='')
+    for file in os.listdir(TEMPLATE_FOLDER):
+        if os.path.isfile(os.path.join(TEMPLATE_FOLDER, file)):
+            shutil.copy(os.path.join(TEMPLATE_FOLDER, file), file)
+        elif os.path.isdir(os.path.join(TEMPLATE_FOLDER, file)):
+            shutil.copytree(os.path.join(TEMPLATE_FOLDER, file), file)
+    print("Done!")
